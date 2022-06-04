78acae237dac68c6a91b3dfffdc26bed4057aa3d
Eremius <jescheffler@gmail.com>
2022-06-04 17:52:25 -0400

    Final
    This should be the final code to control the cooling loop for the laser cutter cooler.

--- thermo/thermo.ino
+++ thermo/thermo.ino
@@ -1,77 +1,137 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <Smoothed.h>
 
-#define ONE_WIRE_BUS 8
+#define ONE_WIRE_BUS 4
 
 OneWire oneWire(ONE_WIRE_BUS);
-
 DallasTemperature sensors(&oneWire);
+Smoothed <float> mySensor;
 
-/*
-  Temp sensor is on D8
-  Heater relay on D2
-  Cooler relay on D4
-  Pump relay on D6  
- 
-*/
- 
- float Celcius=0;
- float Fahrenheit=0;
- bool heater=false;
- bool pump=false;
- bool cooler=false;
- 
+// Global Variables
+bool heater = false;
+bool pump = false;
+bool cooler = false;
+
+const int PumpPin = 5;
+const int CoolerPin = 2;
+const int HeaterPin = 3;
+const int RunningPin = 6;
+
 void setup(void)
 {
-  pinMode(2, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(6, OUTPUT);
-  digitalWrite(2, LOW);
-  digitalWrite(4, LOW);
-  digitalWrite(6, LOW);
+  pinMode(PumpPin, OUTPUT);
+  pinMode(CoolerPin, OUTPUT);
+  pinMode(HeaterPin, OUTPUT);
+  digitalWrite(PumpPin, LOW);
+  digitalWrite(CoolerPin, LOW);
+  digitalWrite(HeaterPin, LOW);
   Serial.begin(9600);
-  sensors.begin();
+  mySensor.begin(SMOOTHED_AVERAGE, 150);
+  mySensor.clear();
 }
+
 
 void loop(void)
 { 
+  // BEGINNING OF LOGIC STAGE
   sensors.requestTemperatures(); 
-  Celcius=sensors.getTempCByIndex(0);
+  mySensor.add(sensors.getTempFByIndex(0));
+
+  Serial.print("Temp: ");
+  Serial.print(mySensor.get());
+  Serial.print("\n");
   
   // If the temp is low
-  while (Celcius <= 4)
+  if (mySensor.get() <= 55)
   {
-      // Turn on Heater
-      heater=true;
-      // Turn on Pump
-      pump=true;
-  }
-  
-  // If the temp is high
-  
-  while (Celcius >= 22)
+      //Serial.print("Temp is LOW\n");
+      heater = true;
+      pump = true;
+      cooler = false;
+  } 
+  else if (mySensor.get() > 55 && mySensor.get() < 80)
   {
-      // Turn on cooler
-      cooler=true;
-      
-      // Turn on pump
-      pump=true;
+    heater = false;
+    cooler = false;
+    pump = false;
   }
-  
+  else if (mySensor.get() >= 80)
+  {
+    //Serial.print("Temp is HIGH\n");
+    cooler = true;
+    pump = true;
+    heater = false;
+  }
+  else
+  {
+    Serial.print("Temp Reading Error\n");
+  }
+
+  if (digitalRead(RunningPin) == HIGH)
+  {
+    Serial.print("RunningPin is HIGH\n");
+    pump = true;
+  }
+  else { Serial.print("RunningPin is LOW\n"); }
+
+// END OF LOGIC STAGE
+
+
+// BEGINNING OF CONTROL STAGE
   if (heater == true)
-  { digitalWrite(2, HIGH); }
-  else
-  { digitalWrite(2, LOW); }
-  
-  if (cooler == true)
-  { digitalWrite(4, HIGH); }
-  else
-  { digitalWrite(4, LOW); }
-  
+  {
+    // Turn Heater ON if OFF
+    if (digitalRead(HeaterPin) == LOW )
+    {
+      digitalWrite(HeaterPin, HIGH);
+      Serial.print("Heater ON\n");
+    }
+  }
+  else if (heater == false)
+  {
+    if (digitalRead(HeaterPin) == HIGH )
+    {
+      digitalWrite(HeaterPin, LOW); 
+      Serial.print("Heater OFF\n");
+    }
+  }
+
   if (pump == true)
-  { digitalWrite(6, HIGH); }
-  else
-  { digitalWrite(6, LOW); }
+  {
+    if (digitalRead(PumpPin) == LOW )
+      {
+        digitalWrite(PumpPin, HIGH); 
+        Serial.print("Pump ON\n");
+      }
+  }
+  else if (pump == false)
+  {
+    if (digitalRead(PumpPin) == HIGH )
+      {
+        digitalWrite(PumpPin, LOW); 
+        Serial.print("Pump OFF\n");
+      }
+  }
+
+  if (cooler == true)
+  {
+    if (digitalRead(CoolerPin) == LOW )
+      {
+        digitalWrite(CoolerPin, HIGH); 
+        Serial.print("Cooler ON\n");
+      }
+  }
+  else if (cooler == false)
+  {
+    if (digitalRead(CoolerPin) == HIGH )
+    {
+      digitalWrite(CoolerPin, LOW); 
+      Serial.print("Cooler OFF\n");
+    }
+  }
+
+  // END OF CONTROL STAGE
   
-  delay(10000);
+  delay(1000);
 }
